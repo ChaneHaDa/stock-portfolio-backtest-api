870c85252598adcd1ec8e723579b86a8e5eefe7c
ChaneHaDa <chane.ha.da@gmail.com>
2024-10-24 12:24:57 +0900

    initial

---
+++ src/main/java/com/chan/stock_portfolio_backtest_api/StockPortfolioBacktestApiApplication.java
@@ -0,0 +1,13 @@
+package com.chan.stock_portfolio_backtest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioBacktestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioBacktestApiApplication.class, args);
+	}
+
+}
