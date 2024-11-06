6d85c87ae598f832534f93126bf3e2afeb650827
ChaneHaDa <chane.ha.da@gmail.com>
2024-11-06 10:12:51 +0900

    feat: change code style

--- src/main/java/com/chan/stock_portfolio_backtest_api/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/exception/CustomizedResponseEntityExceptionHandler.java
@@ -12,16 +12,17 @@ public class CustomizedResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
-                ex.getMessage(), request.getDescription(false));
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+                request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
-        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
-                ex.getMessage(), request.getDescription(false));
+    public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request)
+            throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(),
+                request.getDescription(false));
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 
