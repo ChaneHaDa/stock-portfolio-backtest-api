69d62e9a1510c42e5b64c29a9e1762a46ef8e805
ChaneHaDa <chane.ha.da@gmail.com>
2025-01-22 22:38:36 +0900

    feat: modify http method

--- src/main/java/com/chan/stock_portfolio_backtest_api/config/SecurityConfig.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.chan.stock_portfolio_backtest_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -21,9 +22,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(requests -> requests
-                .requestMatchers("/api-docs", "/swagger-ui/**", "/swagger-resources/**", "/api/v3/**",
-                        "/api/v1/users").permitAll()
-//                .requestMatchers("/h2-console/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                .requestMatchers("/api-docs",
+                        "/swagger-ui/**",
+                        "/swagger-resources/**",
+                        "/api/v3/**",
+                        "/api/v1/users/login").permitAll()
+                .requestMatchers("/h2-console/**").hasRole("ADMIN")
                 .anyRequest().authenticated());
 
         http.formLogin(Customizer.withDefaults());
