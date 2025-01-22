7d8c2bf0b44ad187ad7ac2f7a3503d296bb32160
ChaneHaDa <chane.ha.da@gmail.com>
2025-01-22 22:33:52 +0900

    feat: add login dto

---
+++ src/main/java/com/chan/stock_portfolio_backtest_api/dto/input/LoginDTO.java
@@ -0,0 +1,15 @@
+package com.chan.stock_portfolio_backtest_api.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDTO {
+    private String id;
+    private String password;
+}
