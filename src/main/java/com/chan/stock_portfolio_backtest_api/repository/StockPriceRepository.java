65c0d470c3d1176bc50ec7a795d6c41fda1c562a
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-05 23:16:17 +0900

    feat: id를 사용하는 메소드로 변경

--- src/main/java/com/chan/stock_portfolio_backtest_api/repository/StockPriceRepository.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/repository/StockPriceRepository.java
@@ -1,22 +1,21 @@
 package com.chan.stock_portfolio_backtest_api.repository;
 
 import com.chan.stock_portfolio_backtest_api.domain.StockPrice;
-
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface StockPriceRepository extends JpaRepository<StockPrice, Integer> {
-    @Query("SELECT sp FROM StockPrice sp WHERE sp.stock.name = :name")
-    List<StockPrice> findByStockName(@Param("name") String name);
+import java.time.LocalDate;
+import java.util.List;
 
-    @Query("SELECT sp FROM StockPrice sp WHERE sp.stock.name = :name " +
+public interface StockPriceRepository extends JpaRepository<StockPrice, Integer> {
+    @Query("SELECT sp FROM StockPrice sp WHERE sp.stock.id = :id")
+    List<StockPrice> findByStockId(@Param("id") Integer id);
+
+    @Query("SELECT sp FROM StockPrice sp WHERE sp.stock.id = :id " +
             "AND sp.baseDate >= :startDate " +
             "AND sp.baseDate <= :endDate")
-    List<StockPrice> findByStockNameAndDateRange(@Param("name") String name,
-                                                 @Param("startDate") LocalDate startDate,
-                                                 @Param("endDate") LocalDate endDate);
+    List<StockPrice> findByStockIdAndDateRange(@Param("id") Integer id,
+                                               @Param("startDate") LocalDate startDate,
+                                               @Param("endDate") LocalDate endDate);
 }
