72aa134c5e6601014fcfb5320e0c72ba4101e9a2
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-05 22:38:46 +0900

    feat: id를 사용한 find 함수 추가

--- src/main/java/com/chan/stock_portfolio_backtest_api/service/IndexInfoService.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/service/IndexInfoService.java
@@ -1,7 +1,7 @@
 package com.chan.stock_portfolio_backtest_api.service;
 
-import com.chan.stock_portfolio_backtest_api.dto.request.IndexInfoRequestDTO;
-import com.chan.stock_portfolio_backtest_api.dto.request.IndexPriceRequestDTO;
+import com.chan.stock_portfolio_backtest_api.dto.response.IndexInfoResponseDTO;
+import com.chan.stock_portfolio_backtest_api.dto.response.IndexPriceResponseDTO;
 import com.chan.stock_portfolio_backtest_api.repository.IndexInfoRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +15,13 @@ public class IndexInfoService {
         this.indexInfoRepository = indexInfoRepository;
     }
 
-    public IndexInfoRequestDTO findIndexInfoByName(String name) {
-        return IndexInfoRequestDTO.entityToDTO(indexInfoRepository.findByName(name));
+    public IndexInfoResponseDTO findIndexInfoById(Integer id) {
+        return IndexInfoResponseDTO.entityToDTO(indexInfoRepository.findById(id).orElse(null));
     }
 
-    public IndexPriceRequestDTO findLastIndexPriceByName(String name) {
-        return IndexInfoRequestDTO.entityToDTO(indexInfoRepository.findByName(name)).getIndexPriceList().stream()
-                .max(Comparator.comparing(IndexPriceRequestDTO::getBaseDate))
+    public IndexPriceResponseDTO findLastIndexPriceById(Integer id) {
+        return IndexInfoResponseDTO.entityToDTO(indexInfoRepository.findById(id).orElse(null)).getIndexPriceList().stream()
+                .max(Comparator.comparing(IndexPriceResponseDTO::getBaseDate))
                 .orElse(null);
     }
 }
