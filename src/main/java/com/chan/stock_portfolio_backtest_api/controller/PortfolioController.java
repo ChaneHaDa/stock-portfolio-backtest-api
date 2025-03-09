75bb7c8ab91c35590ec9749da5a2cf3b287f10b1
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-09 22:17:37 +0900

    feat: portfolio 저장 추가

--- src/main/java/com/chan/stock_portfolio_backtest_api/controller/PortfolioController.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/controller/PortfolioController.java
@@ -1,9 +1,12 @@
 package com.chan.stock_portfolio_backtest_api.controller;
 
 import com.chan.stock_portfolio_backtest_api.dto.request.PortfolioBacktestRequestDTO;
+import com.chan.stock_portfolio_backtest_api.dto.request.PortfolioRequestDTO;
 import com.chan.stock_portfolio_backtest_api.dto.response.PortfolioBacktestResponseDTO;
+import com.chan.stock_portfolio_backtest_api.dto.response.PortfolioResponseDTO;
 import com.chan.stock_portfolio_backtest_api.dto.response.ResponseDTO;
 import com.chan.stock_portfolio_backtest_api.service.PortfolioBacktestService;
+import com.chan.stock_portfolio_backtest_api.service.PortfolioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -20,9 +23,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Portfolio API", description = "포트폴리오 백테스트 API")
 public class PortfolioController {
     private final PortfolioBacktestService portfolioBacktestService;
+    private final PortfolioService portfolioService;
 
-    public PortfolioController(PortfolioBacktestService portfolioBacktestService) {
+    public PortfolioController(PortfolioBacktestService portfolioBacktestService, PortfolioService portfolioService) {
         this.portfolioBacktestService = portfolioBacktestService;
+        this.portfolioService = portfolioService;
     }
 
     @PostMapping("/backtest")
@@ -44,22 +49,24 @@ public class PortfolioController {
     }
 
     @PostMapping
-    @Operation(summary = "포트폴리오 저장", description = "사용자가 만든 포트폴리오를 저장합니다.")
+    @Operation(summary = "포트폴리오 저장", description = "사용자가 만든 포트폴리오를 저장하고 결과를 반환합니다.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "백테스팅 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 입력값")
+            @ApiResponse(responseCode = "201", description = "포트폴리오 저장 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력값으로 저장 실패"),
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류")
     })
     public ResponseEntity<ResponseDTO<?>> savePortfolio(
-            @RequestBody @Valid PortfolioBacktestRequestDTO portfolioBacktestRequestDTO
+            @RequestBody @Valid PortfolioRequestDTO portfolioRequestDTO
     ) {
 
+        PortfolioResponseDTO savedPortfolio = portfolioService.createPortfolio(portfolioRequestDTO);
 
-//        ResponseDTO<?> response = ResponseDTO.<>builder()
-//                .status("success")
-//                .data(result)
-//                .build();
+        ResponseDTO<PortfolioResponseDTO> response = ResponseDTO.<PortfolioResponseDTO>builder()
+                .status("success")
+                .data(savedPortfolio)
+                .build();
 
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(response);
     }
 
 }
