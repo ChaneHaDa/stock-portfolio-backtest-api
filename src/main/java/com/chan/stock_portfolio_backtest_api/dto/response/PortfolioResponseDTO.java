4e64d6255e08c1a233abc98fc27743b3d3c9dbe4
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-09 22:15:10 +0900

    feat: PortfolioResponseDTO 추가

---
+++ src/main/java/com/chan/stock_portfolio_backtest_api/dto/response/PortfolioResponseDTO.java
@@ -0,0 +1,36 @@
+package com.chan.stock_portfolio_backtest_api.dto.response;
+
+import com.chan.stock_portfolio_backtest_api.domain.Portfolio;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PortfolioResponseDTO {
+    private String name;
+    private String description;
+    private Long amount;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Float ror;
+    private Float volatility;
+    private Float price;
+
+    public static PortfolioResponseDTO entityToDTO(Portfolio portfolio) {
+        return PortfolioResponseDTO.builder()
+                .name(portfolio.getName())
+                .description(portfolio.getDescription())
+                .amount(portfolio.getAmount())
+                .startDate(portfolio.getStartDate())
+                .endDate(portfolio.getEndDate())
+                .ror(portfolio.getRor())
+                .volatility(portfolio.getVolatility())
+                .price(portfolio.getPrice())
+                .build();
+    }
+
+}
