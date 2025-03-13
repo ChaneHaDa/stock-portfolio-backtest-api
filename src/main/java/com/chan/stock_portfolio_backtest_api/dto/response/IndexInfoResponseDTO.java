d8584f3bba6548f56d1c411696317bfb7552b21e
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-13 22:14:04 +0900

    feat: remove unuse code

--- src/main/java/com/chan/stock_portfolio_backtest_api/dto/response/IndexInfoResponseDTO.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/dto/response/IndexInfoResponseDTO.java
@@ -3,8 +3,6 @@ package com.chan.stock_portfolio_backtest_api.dto.response;
 import com.chan.stock_portfolio_backtest_api.domain.IndexInfo;
 import lombok.*;
 
-import java.util.List;
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,14 +12,12 @@ public class IndexInfoResponseDTO {
     private Integer id;
     private String name;
     private String category;
-    private List<IndexPriceResponseDTO> indexPriceList;
 
     public static IndexInfoResponseDTO entityToDTO(IndexInfo indexInfo) {
         return IndexInfoResponseDTO.builder()
                 .id(indexInfo.getId())
                 .name(indexInfo.getName())
                 .category(indexInfo.getCategory())
-                .indexPriceList(indexInfo.getIndexPriceList().stream().map(IndexPriceResponseDTO::entityToDTO).toList())
                 .build();
     }
 }
