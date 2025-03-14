602d1ce3a88d9c38881afde45afc04289ae2d85e
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-15 00:24:46 +0900

    feat: getCurrentUser 예외처리 추가

--- src/main/java/com/chan/stock_portfolio_backtest_api/service/AuthService.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.chan.stock_portfolio_backtest_api.service;
 
 import com.chan.stock_portfolio_backtest_api.domain.Users;
+import com.chan.stock_portfolio_backtest_api.exception.EntityNotFoundException;
+import com.chan.stock_portfolio_backtest_api.exception.UnauthorizedException;
 import com.chan.stock_portfolio_backtest_api.repository.UsersRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,7 +19,21 @@ public class AuthService {
 
     public Users getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return usersRepository.findByUsername(authentication.getName());
+
+        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getPrincipal())) {
+            throw new UnauthorizedException("인증되지 않은 사용자입니다.");
+        }
+        String username = authentication.getName();
+        if (username == null || username.isEmpty()) {
+            throw new UnauthorizedException("사용자 이름이 유효하지 않습니다.");
+        }
+
+        Users user = usersRepository.findByUsername(authentication.getName());
+        if (user == null) {
+            throw new EntityNotFoundException("해당 사용자를 찾을 수 없습니다: " + authentication.getName());
+        }
+
+        return user;
     }
 
 }
