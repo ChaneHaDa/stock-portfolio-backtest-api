1ae28f50de32fee52670609db555f2f706553bb3
ChaneHaDa <chane.ha.da@gmail.com>
2025-03-20 23:05:06 +0900

    feat: 반환값에 id 추가

--- src/main/java/com/chan/stock_portfolio_backtest_api/controller/PortfolioController.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/controller/PortfolioController.java
@@ -67,7 +67,7 @@ public class PortfolioController {
     }
 
     @GetMapping
-    @Operation(summary = "포트폴리오 조회", description = "로그인한 사용자가 저장한 모든 포트폴리오 리스트 반환")
+    @Operation(summary = "사용자 포트폴리오 리스트 조회", description = "로그인한 사용자가 저장한 모든 포트폴리오 리스트 반환")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "포트폴리오 조회 성공"),
             @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
