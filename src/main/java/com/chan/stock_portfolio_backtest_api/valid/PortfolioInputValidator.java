3d7f7dd8b6bfcd02294388ff544ad6807011df42
ChaneHaDa <chane.ha.da@gmail.com>
2025-06-27 23:01:27 +0900

    feat: 예외 관련 필드명 수정

--- src/main/java/com/chan/stock_portfolio_backtest_api/valid/PortfolioInputValidator.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/valid/PortfolioInputValidator.java
@@ -1,50 +1,52 @@
 package com.chan.stock_portfolio_backtest_api.valid;
 
-import com.chan.stock_portfolio_backtest_api.dto.request.PortfolioBacktestRequestDTO;
-import com.chan.stock_portfolio_backtest_api.dto.request.PortfolioBacktestRequestItemDTO;
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-
 import java.time.LocalDate;
 import java.util.List;
 
+import com.chan.stock_portfolio_backtest_api.dto.request.PortfolioBacktestRequestDTO;
+import com.chan.stock_portfolio_backtest_api.dto.request.PortfolioBacktestRequestItemDTO;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
 public class PortfolioInputValidator implements ConstraintValidator<ValidPortfolioInput, PortfolioBacktestRequestDTO> {
-    @Override
-    public boolean isValid(PortfolioBacktestRequestDTO portfolioBacktestRequestDTO, ConstraintValidatorContext constraintValidatorContext) {
-        constraintValidatorContext.disableDefaultConstraintViolation();
+	@Override
+	public boolean isValid(PortfolioBacktestRequestDTO portfolioBacktestRequestDTO,
+		ConstraintValidatorContext constraintValidatorContext) {
+		constraintValidatorContext.disableDefaultConstraintViolation();
 
-        if (portfolioBacktestRequestDTO == null) {
-            return true;
-        }
+		if (portfolioBacktestRequestDTO == null) {
+			return true;
+		}
 
-        //startDate < endDate valid
-        LocalDate startDate = portfolioBacktestRequestDTO.getStartDate();
-        LocalDate endDate = portfolioBacktestRequestDTO.getEndDate();
-        if (startDate != null && endDate != null) {
-            if (!startDate.isBefore(endDate)) {
-                constraintValidatorContext
-                        .buildConstraintViolationWithTemplate("endDate must be after the startDate")
-                        .addPropertyNode("endDate")
-                        .addConstraintViolation();
-                return false;
-            }
-        }
+		//startDate < endDate valid
+		LocalDate startDate = portfolioBacktestRequestDTO.getStartDate();
+		LocalDate endDate = portfolioBacktestRequestDTO.getEndDate();
+		if (startDate != null && endDate != null) {
+			if (!startDate.isBefore(endDate)) {
+				constraintValidatorContext
+					.buildConstraintViolationWithTemplate("endDate must be after the startDate")
+					.addPropertyNode("endDate")
+					.addConstraintViolation();
+				return false;
+			}
+		}
 
-        //weight sum == 1 valid
-        List<PortfolioBacktestRequestItemDTO> portfolioBacktestRequestItemDTOList = portfolioBacktestRequestDTO.getPortfolioBacktestRequestItemDTOList();
-        float weightSum = 0;
-        for (PortfolioBacktestRequestItemDTO i : portfolioBacktestRequestItemDTOList) {
-            weightSum += i.getWeight();
-        }
+		//weight sum == 1 valid
+		List<PortfolioBacktestRequestItemDTO> portfolioBacktestRequestItemDTOList = portfolioBacktestRequestDTO.getPortfolioBacktestRequestItemDTOList();
+		float weightSum = 0;
+		for (PortfolioBacktestRequestItemDTO i : portfolioBacktestRequestItemDTOList) {
+			weightSum += i.getWeight();
+		}
 
-        if (weightSum != 1) {
-            constraintValidatorContext
-                    .buildConstraintViolationWithTemplate("weight sum is might be 1")
-                    .addPropertyNode("portfolioInputItemDTOList")
-                    .addConstraintViolation();
-            return false;
-        }
+		if (weightSum != 1) {
+			constraintValidatorContext
+				.buildConstraintViolationWithTemplate("weight sum is might be 1")
+				.addPropertyNode("portfolioBacktestRequestItemDTOList")
+				.addConstraintViolation();
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
