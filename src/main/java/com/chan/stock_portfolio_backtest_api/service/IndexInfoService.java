a5fb3ac6b4daadaba7a8d0cde996995129445871
ChaneHaDa <chane.ha.da@gmail.com>
2025-06-27 22:58:30 +0900

    feat: 엔티티 없는 경우 예외처리 추가

--- src/main/java/com/chan/stock_portfolio_backtest_api/service/IndexInfoService.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/service/IndexInfoService.java
@@ -1,18 +1,21 @@
 package com.chan.stock_portfolio_backtest_api.service;
 
-import com.chan.stock_portfolio_backtest_api.dto.response.IndexInfoResponseDTO;
-import com.chan.stock_portfolio_backtest_api.repository.IndexInfoRepository;
 import org.springframework.stereotype.Service;
+
+import com.chan.stock_portfolio_backtest_api.dto.response.IndexInfoResponseDTO;
+import com.chan.stock_portfolio_backtest_api.exception.EntityNotFoundException;
+import com.chan.stock_portfolio_backtest_api.repository.IndexInfoRepository;
 
 @Service
 public class IndexInfoService {
-    private final IndexInfoRepository indexInfoRepository;
+	private final IndexInfoRepository indexInfoRepository;
 
-    public IndexInfoService(IndexInfoRepository indexInfoRepository) {
-        this.indexInfoRepository = indexInfoRepository;
-    }
+	public IndexInfoService(IndexInfoRepository indexInfoRepository) {
+		this.indexInfoRepository = indexInfoRepository;
+	}
 
-    public IndexInfoResponseDTO findIndexInfoById(Integer id) {
-        return IndexInfoResponseDTO.entityToDTO(indexInfoRepository.findById(id).orElse(null));
-    }
+	public IndexInfoResponseDTO findIndexInfoById(Integer id) {
+		return IndexInfoResponseDTO.entityToDTO(indexInfoRepository.findById(id)
+			.orElseThrow(() -> new EntityNotFoundException("해당 Entity가 없습니다.")));
+	}
 }
