a043b40ef7d6202797314fc1d71b0b903e66db2e
ChaneHaDa <chane.ha.da@gmail.com>
2025-07-22 22:30:50 +0900

    feat: 날짜 예외 처리

--- src/main/java/com/chan/stock_portfolio_backtest_api/service/StockPriceService.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/service/StockPriceService.java
@@ -1,44 +1,51 @@
 package com.chan.stock_portfolio_backtest_api.service;
 
-import com.chan.stock_portfolio_backtest_api.dto.request.StockPriceRequestDTO;
-import com.chan.stock_portfolio_backtest_api.exception.EntityNotFoundException;
-import com.chan.stock_portfolio_backtest_api.repository.StockPriceRepository;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.chan.stock_portfolio_backtest_api.dto.request.StockPriceRequestDTO;
+import com.chan.stock_portfolio_backtest_api.exception.EntityNotFoundException;
+import com.chan.stock_portfolio_backtest_api.repository.StockPriceRepository;
+
 @Service
 public class StockPriceService {
-    private final StockPriceRepository stockPriceRepository;
+	private final StockPriceRepository stockPriceRepository;
 
-    public StockPriceService(StockPriceRepository stockPriceRepository) {
-        this.stockPriceRepository = stockPriceRepository;
-    }
+	public StockPriceService(StockPriceRepository stockPriceRepository) {
+		this.stockPriceRepository = stockPriceRepository;
+	}
 
-    public List<StockPriceRequestDTO> findStockPricesByStockId(Integer id) {
-        List<StockPriceRequestDTO> stockPriceRequestDTOList = stockPriceRepository.findByStockId(id)
-                .stream()
-                .map(StockPriceRequestDTO::entityToDTO)
-                .toList();
+	public List<StockPriceRequestDTO> findStockPricesByStockId(Integer id) {
+		List<StockPriceRequestDTO> stockPriceRequestDTOList = stockPriceRepository.findByStockId(id)
+			.stream()
+			.map(StockPriceRequestDTO::entityToDTO)
+			.toList();
 
-        if (stockPriceRequestDTOList.isEmpty()) {
-            throw new EntityNotFoundException(String.format("%s not found", id));
-        }
+		if (stockPriceRequestDTOList.isEmpty()) {
+			throw new EntityNotFoundException(String.format("%s not found", id));
+		}
 
-        return stockPriceRequestDTOList;
-    }
+		return stockPriceRequestDTOList;
+	}
 
-    public List<StockPriceRequestDTO> findStockPricesByStockIdAndDateRange(Integer id, LocalDate startDate, LocalDate endDate) {
-        List<StockPriceRequestDTO> stockPriceRequestDTOList = stockPriceRepository.findByStockIdAndDateRange(id, startDate, endDate)
-                .stream()
-                .map(StockPriceRequestDTO::entityToDTO)
-                .toList();
+	public List<StockPriceRequestDTO> findStockPricesByStockIdAndDateRange(Integer id, LocalDate startDate,
+		LocalDate endDate) {
+		if (startDate.isAfter(endDate)) {
+			throw new IllegalArgumentException("Start date must not be after end date.");
+		}
 
-        if (stockPriceRequestDTOList.isEmpty()) {
-            throw new EntityNotFoundException(String.format("id: %s not found", id));
-        }
+		List<StockPriceRequestDTO> stockPriceRequestDTOList = stockPriceRepository.findByStockIdAndDateRange(id,
+				startDate, endDate)
+			.stream()
+			.map(StockPriceRequestDTO::entityToDTO)
+			.toList();
 
-        return stockPriceRequestDTOList;
-    }
+		if (stockPriceRequestDTOList.isEmpty()) {
+			throw new EntityNotFoundException(String.format("id: %s not found", id));
+		}
+
+		return stockPriceRequestDTOList;
+	}
 }
