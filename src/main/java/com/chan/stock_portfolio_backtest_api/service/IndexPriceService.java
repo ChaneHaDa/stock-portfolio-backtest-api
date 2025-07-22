6dc60692691c469c32fbf157753fa16afbc5931e
ChaneHaDa <chane.ha.da@gmail.com>
2025-07-22 22:30:21 +0900

    feat: 날짜 예외 처리

--- src/main/java/com/chan/stock_portfolio_backtest_api/service/IndexPriceService.java
+++ src/main/java/com/chan/stock_portfolio_backtest_api/service/IndexPriceService.java
@@ -1,45 +1,52 @@
 package com.chan.stock_portfolio_backtest_api.service;
 
-import com.chan.stock_portfolio_backtest_api.dto.response.IndexPriceResponseDTO;
-import com.chan.stock_portfolio_backtest_api.exception.EntityNotFoundException;
-import com.chan.stock_portfolio_backtest_api.repository.IndexPriceRepository;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.chan.stock_portfolio_backtest_api.dto.response.IndexPriceResponseDTO;
+import com.chan.stock_portfolio_backtest_api.exception.EntityNotFoundException;
+import com.chan.stock_portfolio_backtest_api.repository.IndexPriceRepository;
+
 @Service
 public class IndexPriceService {
-    private final IndexPriceRepository indexPriceRepository;
+	private final IndexPriceRepository indexPriceRepository;
 
-    public IndexPriceService(IndexPriceRepository indexPriceRepository) {
-        this.indexPriceRepository = indexPriceRepository;
-    }
+	public IndexPriceService(IndexPriceRepository indexPriceRepository) {
+		this.indexPriceRepository = indexPriceRepository;
+	}
 
-    public List<IndexPriceResponseDTO> findIndexPriceByIndexId(Integer stockId) {
-        List<IndexPriceResponseDTO> indexPriceResponseDTOList = indexPriceRepository.findAllByIndexInfo_Id(stockId)
-                .stream()
-                .map(IndexPriceResponseDTO::entityToDTO)
-                .toList();
+	public List<IndexPriceResponseDTO> findIndexPriceByIndexId(Integer stockId) {
+		List<IndexPriceResponseDTO> indexPriceResponseDTOList = indexPriceRepository.findAllByIndexInfo_Id(stockId)
+			.stream()
+			.map(IndexPriceResponseDTO::entityToDTO)
+			.toList();
 
-        if (indexPriceResponseDTOList.isEmpty()) {
-            throw new EntityNotFoundException("index price not found");
-        }
+		if (indexPriceResponseDTOList.isEmpty()) {
+			throw new EntityNotFoundException("index price not found");
+		}
 
-        return indexPriceResponseDTOList;
-    }
+		return indexPriceResponseDTOList;
+	}
 
-    public List<IndexPriceResponseDTO> findIndexPriceByIndexInfoIdAndDateRange(Integer id, LocalDate startDate, LocalDate endDate) {
-        List<IndexPriceResponseDTO> indexPriceResponseDTOList = indexPriceRepository.findByIndexInfoIdAndDateRange(id, startDate, endDate)
-                .stream()
-                .map(IndexPriceResponseDTO::entityToDTO)
-                .toList();
+	public List<IndexPriceResponseDTO> findIndexPriceByIndexInfoIdAndDateRange(Integer id, LocalDate startDate,
+		LocalDate endDate) {
+		if (startDate.isAfter(endDate)) {
+			throw new IllegalArgumentException("Start date must not be after end date.");
+		}
 
-        if (indexPriceResponseDTOList.isEmpty()) {
-            throw new EntityNotFoundException("index price not found");
-        }
+		List<IndexPriceResponseDTO> indexPriceResponseDTOList = indexPriceRepository.findByIndexInfoIdAndDateRange(id,
+				startDate, endDate)
+			.stream()
+			.map(IndexPriceResponseDTO::entityToDTO)
+			.toList();
 
-        return indexPriceResponseDTOList;
-    }
+		if (indexPriceResponseDTOList.isEmpty()) {
+			throw new EntityNotFoundException("index price not found");
+		}
+
+		return indexPriceResponseDTOList;
+	}
 
 }
